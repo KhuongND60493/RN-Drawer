ed69ac6aabeb81646219b29bc76d357c34efdbaf
khuongnd60493 <nguyenromio@gmail.com>
2018-08-14 11:02:12 +0700

    khuongnd5_update_use_drawer

---
+++ OptionDrawers.js
@@ -0,0 +1,14 @@
+const OptionDrawers = {
+	position: {
+		left: 'left',
+		right: 'right',
+		top: 'top',
+		bottom: 'bottom'
+	},
+	type: {
+		overlay: 'overlay',
+		static: 'static',
+		displace: 'displace'
+	}
+}
+export default OptionDrawers
